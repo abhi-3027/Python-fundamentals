b49eb3b438436790ed762d56eade6e08a7910afa
Abhishek Sharma <60639409+abhi-3027@users.noreply.github.com>
2021-05-09 13:53:14 +0530

    variables and values in python

---
+++ variables_and_values.py
@@ -0,0 +1,27 @@
+# Variables and Values
+
+'''
+Variables  ---> references to the values in the memory
+
+varidbles can be  strings
+				  integers
+				  floating point values
+				  booleans (true or false)
+				  different data types and data structures (list, tuples, dict)
+
+values can be  numeric values - > integer (e.g. 7) ,  floating point numbers (e.g. 7.1), 
+			   string values
+
+
+Operations on integers
+ +  --> addition
+ -  --> subtraction
+ /  --> division
+ *  --> multiplication
+ %  --> modulus (remainder after division)
+ ** --> exponet (e.g. 2**3 = 8)
+ // --> floor division (e.g. 9//2 = 4,  9//2.0 = 4.0)
+
+
+ Strings --> A sequence of characters that's placed in between two quotation marks.
+ '''
